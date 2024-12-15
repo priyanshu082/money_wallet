9790e18518cc57c57b932c8f81bf8757282d7c34
priyanshu082 <priyanshusingh216@gmail.com>
2024-12-15 13:40:52 +0530

    PROJECT :: CLERK AUTH IS SETUP IN MERCHANT AND USER APP

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,10 +1,22 @@
-import styles from "./page.module.css";
+import styles from "./page.module.css"
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 
 
 export default function Home() {
   return (
     <div className={styles.page}>
+       <SignedOut>
+      <SignInButton />
+    </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       MERCHANT_APP
     </div>
   );
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,25 @@
 import {PrismaClient} from "@repo/prisma/client"
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const client=new PrismaClient();
 
 export default function Home() {
   return (
-    <div className="bg-yellow-400 h-[100vh]">
-     hello
-      
+    <div className=" h-[100vh] flex justify-center flex-col items-center">
+ <SignedOut>
+      <SignInButton />
+    </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+<div>
+      USER_APP
+</div>
     </div>
   );
 }
