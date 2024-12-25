54e9862dd0937a4a3dcb69a45c46d7a904b507f1
priyanshu082 <priyanshusingh216@gmail.com>
2024-12-25 09:35:17 +0530

    PROJECT :: Balace card and recent transacations are done

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { AddMoneyCard } from '../../../components/AddMoneyCard'
+import BalanceCard from '../../../components/BalanceCard'
 
 const page = () => {
   return (
     <div>
         <AddMoneyCard/>
+        {/* <BalanceCard /> */}
     </div>
   )
 }
---
+++ apps/user-app/components/OnRampTransaction.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Card from '@repo/ui/card'
+
+// Define possible transaction statuses
+export type TransactionStatus = 'Success' | 'Failure' | 'Processing'
+
+// Interface for a single transaction
+export interface ITransaction {
+    time: Date;
+    amount: number;
+    status: TransactionStatus;
+    provider: string;
+}
+
+// Interface for component props
+export interface IOnRampTransactionProps {
+    transactions: ITransaction[] | null;
+}
+
+const OnRampTransaction: React.FC<IOnRampTransactionProps> = ({ transactions }) => {
+    if (!transactions) {
+        return <Card title="Recent Transactions">
+            <div className="text-center pb-8 pt-8">
+                No Recent transactions
+            </div>
+        </Card>
+    }
+
+    return <Card title="Recent Transactions">
+        <div className="pt-2">
+            {transactions.map((t: ITransaction, index: number) => (
+                <div key={index} className="flex justify-between">
+                    <div>
+                        <div className="text-sm">
+                            Received INR
+                        </div>
+                        <div className="text-slate-600 text-xs">
+                            {t.time.toDateString()}
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-center">
+                        + Rs {t.amount / 100}
+                    </div>
+                </div>
+            ))}
+        </div>
+    </Card>
+}
+
+export default OnRampTransaction
